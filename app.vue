5dc7778b7b08599bfa99a57069a3f45ac12d04f5
wylited <wylited@gmail.com>
2022-10-05 15:15:40 +0800

    adding a sidebar to the shit

--- app.vue
+++ app.vue
@@ -2,17 +2,19 @@
 <template>
   <div>
 
+<div class="drawer">
+  <input id="my-drawer" type="checkbox" class="drawer-toggle" />
+  <div class="drawer-content">
+    <!-- Page content here -->
+
     <!-- navbar -->
     <div class="navbar bg-base-100">
 
         <div class="flex-none">
-            <button class="btn btn-square btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-10 h-10 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-            </button>
+            <label for="my-drawer" class="btn btn-square btn-ghost drawer-button"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-10 h-10 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg></label>
         </div>
 
-        <div class="flex-1">
-            
+        <div class="flex-1"> 
             <a class="btn btn-ghost normal-case text-2xl">club.dev</a>
         </div>
         <div class="flex-none">
@@ -45,7 +47,20 @@
           <button class="btn btn-primary">Get Started</button>
         </div>
       </div>
-    </div>
+    </div>    
+
+  </div> 
+  <div class="drawer-side">
+    <label for="my-drawer" class="drawer-overlay"></label>
+    <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+      <!-- Sidebar content here -->
+      <li><a>Sidebar Item 1</a></li>
+      <li><a>Sidebar Item 2</a></li>
+      
+    </ul>
+  </div>
+</div>
+
 
   </div>
 </template>
