5cbb8074549dbe67fa0d02338d93f6cd8a0336a1
Clydexy <xuj1@kgv.hk>
2022-10-06 22:06:26 +0800

    Added Personal Team Cards

--- pages/Team.vue
+++ pages/Team.vue
@@ -1,5 +1,55 @@
 <template>
-  <div>
-      
-  </div>
+  <section class="container mx-auto py-36 px-8">
+  <div class="flex justify items-center gap-10 flex-col md:flex-row">
+    <div class="card w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">Alex Climie</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions justify-end">
+          <button class="btn btn-primary">Contact</button>
+        </div>
+      </div>
+    </div>
+    <div class="card w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">Dhairya Shah</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions justify-end">
+          <button class="btn btn-primary">Contact</button>
+        </div>
+      </div>
+    </div>
+    <div class="card w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">Erik Xu</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions justify-end">
+          <button class="btn btn-primary">Contact</button>
+        </div>
+      </div>
+    </div>
+    <div class="card w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">Ethan Cheung</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions justify-end">
+          <button class="btn btn-primary">Contact</button>
+        </div>
+      </div>
+    </div>
+    </div>
+  </section>
+  
 </template>
