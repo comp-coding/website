25446a20536a8b85c0ebdf1b494ae0d70cb1c6f7
wylited <wylited@gmail.com>
2022-10-06 17:59:16 +0800

    update introduction

--- components/Hero.vue
+++ components/Hero.vue
@@ -10,7 +10,10 @@
             </div>
             <div>
                 <h1 class="text-5xl font-bold">Welcome to club.dev!</h1>
-                <p class="py-6">This club is supported by KGV CAS and Hack Club, a nonprofit organisation that provides us with many resources. This club is expected to start shortly after Challenge Week 2022, and signups will be available on Lionel soon. The website is still under development.</p>
+                
+                <p class="py-6">
+                    club.dev is founded by a group of y11 computer science students aiming to foster and create a society that focuses on development of real world skills that can be marketed. This club is supported by KGV CAS and Hack Club, a nonprofit organisation that provides us with many resources. This club is also expected to start shortly after Challenge Week 2022, and signups will be available on Lionel soon. The website is still under development.
+                </p>
                 <button class="btn btn-primary">Get Started</button>
             </div>
         </div>
