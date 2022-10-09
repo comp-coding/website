91988f68c27b224dbc1ca157f27944fe6cbeb5f4
Alex Climie <55697723+13carpileup@users.noreply.github.com>
2022-10-09 15:43:21 +0800

    rules restructure

--- pages/Rules.vue
+++ pages/Rules.vue
@@ -2,49 +2,49 @@
   <div class="mockup-window border-2 border-base-300 bg-base-100 flex-col">
     <div class="m-10">
       <h1 class="mb-3 text-5xl font-bold">Rules</h1>
-      <p class="mb-3 text-3xl">Our expectations from you</p>
-      <p class="mb-3">
-        This society focuses on development of projects as well as real world
-        skills required that can be marketed easily to help prepare for the
-        future.
-      </p>
-      <p class="mb-3 text-2xl">Respect:</p>
-      <p class="mb-3">
-        We expect you to respect everyone in the society. This includes
-        respecting the opinions of others, respecting the time of others, and
-        respecting the rules of the society.
-      </p>
-      <p class="mb-3 text-2xl">Participation:</p>
-      <p class="mb-3">
-        We expect you to participate in the society. This includes participating
-        in society events, participating in society projects, and participating
-        in society discussions.
-      </p>
-      <p class="mb-3 text-2xl">Contribution:</p>
-      <p class="mb-3">
-        We expect you to contribute to the society. This includes contributing to
-        society events, contributing to society projects, and contributing to
-        society discussions.
-      </p>
-      <p class="mb-3 text-2xl">Responsibility:</p>
-      <p class="mb-3">
-        We expect you to be responsible for your actions. This includes being
-        responsible for your actions in the society, being responsible for your
-        actions in society events, being responsible for your actions in society
-        projects, and being responsible for your actions in society discussions.
-      </p>
-      <p class="mb-3 text-2xl">Safety:</p>
-      <p class="mb-3">
-        We expect you to be safe. This includes being safe in society events,
-        being safe in society projects, and being safe in society discussions.
-      </p>
-      <p class="mb-3 text-2xl">Privacy:</p>
-      <p class="mb-3">
-        We expect you to respect the privacy of others. This includes respecting
-        the privacy of others in society events, respecting the privacy of
-        others in society projects, and respecting the privacy of others in
-        society discussions.
-      </p>
+      <p class="mb-3 text-3xl">Our expectations from you:</p>
+      <br>
+
+      <div v-for="item in items" v-bind:key="item.id">
+      <p class="mb-3 text-2xl">{{item.rule}}</p>
+      <p class="mb-3">{{item.expl}}</p>
+      </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          id: 1,
+          rule: "Respect",
+          expl: "We expect you to respect everyone in the society. This includes respecting the opinions of others, respecting the time of others, and respecting the rules of the society.",
+        },
+        {
+          id: 2,
+          rule: "Participation",
+          expl: "We expect you to participate in the society. This includes participating in society events, participating in society projects, and participating in society discussions.",
+        },
+        {
+          id: 3,
+          rule: "Contribution",
+          expl: "We expect you to contribute to the society. This includes contributing to society events, contributing to society projects, and contributing to society discussions.",
+        },
+        {
+          id: 4,
+          rule: "Responsibility",
+          expl: "We expect you to be responsible for your actions. This includes being responsible for your actions in the society, being responsible for your actions in society events, being responsible for your actions in society projects, and being responsible for your actions in society discussions.",
+        },
+        {
+          id: 5,
+          rule: "Privacy",
+          expl: "We expect you to respect the privacy of others. This includes respecting the privacy of others in society events, respecting the privacy of others in society projects, and respecting the privacy of others in society discussions.",
+        },
+      ]
+    }
+  }
+}
+</script>
