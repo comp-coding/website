86fc17130c9357c901d8e783d6a6eb9111c47189
Alex Climie <55697723+13carpileup@users.noreply.github.com>
2022-10-10 21:36:20 +0800

    faq clean

--- pages/FAQ.vue
+++ pages/FAQ.vue
@@ -1,29 +1,17 @@
 <template>
-  <div class="w-11/12 content-center mx-auto mb-10 transition-colors ease-linear shadow-md w-11/12">
-    <div class="w-full h-12 rounded-t-lg bg-gray-200 dark:bg-gray-900 flex justify-start items-center space-x-1.5 px-4 ">
-        <span class="w-3 h-3 border-2 border-transparent dark:border-red-400 rounded-full bg-red-400 dark:bg-transparent "></span>
-        <span class="w-3 h-3 border-2 border-transparent dark:border-yellow-400 rounded-full bg-yellow-400 dark:bg-transparent"></span>
-        <span class="w-3 h-3 border-2 border-transparent dark:border-green-400 rounded-full bg-green-400 dark:bg-transparent"></span>
-    </div>
-  <div class="flex bg-gray-100 dark:bg-gray-700 border-t-0 w-full h-96 rounded-b-lg h-3/6">
-
-            <div class="text-center lg:text-left m-10 justify-center">
-              <h1 class="mb-3 text-5xl font-bold">Frequently Asked Questions </h1>
-
-                    <div class="text-center lg:text-left m-10">
-        <div v-for="item in items" v-bind:key="item.id">
-          <p class="mb-3 text-2xl font-semibold">{{item.question}}</p>
-          <p class="mb-3 font-thin pb-6">
-            {{item.answer}}
-          </p>
-        </div>
-        <h1 class='text-2xl'>Have any more questions? <NuxtLink to='/Contact' class='text-sky-600'>Contact us.</NuxtLink></h1>
-
+  <div class="text-center lg:text-left m-10 justify-center">
+  <h1 class="mb-3 text-5xl font-bold">Frequently Asked Questions </h1>
+    <div class="text-center lg:text-left m-10">
+      <div v-for="item in items" v-bind:key="item.id">
+        <p class="mb-3 text-2xl font-semibold">{{item.question}}</p>
+        <p class="mb-3 font-thin pb-6">
+          {{item.answer}}
+        </p>
       </div>
-      </div>
+      <h1 class='text-2xl'>Have any more questions? <NuxtLink to='/Contact' class='text-sky-600'>Contact us.</NuxtLink></h1>
     </div>
-
-</div>
+  </div>
+ 
 
       
 </template>
